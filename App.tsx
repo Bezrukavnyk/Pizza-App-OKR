fcedfad4d4eb90b85d38e0a5c420ae5d91d5c08d
Ihor Bezrukavnyk <ihor.bezrukavnyk@ideus.biz>
2023-07-19 18:52:42 +0300

    RDAN-9 | added spalsh screen

--- App.tsx
+++ App.tsx
@@ -1,16 +1,22 @@
-import React from "react";
-import { SafeAreaView } from "react-native";
+import React, { useEffect } from "react";
+import { Platform, SafeAreaView } from "react-native";
 import { Provider } from "react-redux";
+import SplashScreen from "react-native-splash-screen";
 
 import { store } from "utils/store/store";
 import Router from "screens/Router/Router";
+const App = (): JSX.Element => {
+  useEffect(() => {
+    if (Platform.OS === "android") SplashScreen?.hide();
+  }, []);
 
-const App = (): JSX.Element => (
-  <SafeAreaView style={{ flex: 1 }}>
-    <Provider store={store}>
-      <Router />
-    </Provider>
-  </SafeAreaView>
-);
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Provider store={store}>
+        <Router />
+      </Provider>
+    </SafeAreaView>
+  );
+};
 
 export default App;
